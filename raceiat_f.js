fcd5c5e93fc93efad86f9588320554ad7315f595
MiranaR31 <miranaranerison@hotmail.fr>
2026-04-07 10:00:50 +0200

    update

--- raceiat_f.js
+++ raceiat_f.js
@@ -6,7 +6,7 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
         category1 : {
             name : 'Arabo-musulmane', //Will appear in the data.
             title : {
-                media : {word : 'Immigrant'}, //Name of the category presented in the task.
+                media : {word : 'Arabo-musulmane'}, //Name of the category presented in the task.
                 css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
@@ -24,7 +24,7 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
         category2 : {
             name : 'Française', //Will appear in the data.
             title : {
-                media : {word : 'Natif'}, //Name of the category presented in the task.
+                media : {word : 'Française'}, //Name of the category presented in the task.
                 css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
