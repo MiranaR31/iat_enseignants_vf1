786a2f0320dc70ccbd38233aac6bae1ba3d77fcc
MiranaR31 <miranaranerison@hotmail.fr>
2026-04-10 10:43:03 +0200

    Update mgr.js

--- mgr.js
+++ mgr.js
@@ -9,7 +9,6 @@ define(['managerAPI',
 
 	var API    = new Manager();
 	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-	init_data_pipe(API, '4mlbP4UaqvIv',  {file_type:'csv'});	
 	init_data_pipe(API, '0FHB6J9Gv5MR',  {file_type:'csv'});	
 
     API.setName('mgr');
